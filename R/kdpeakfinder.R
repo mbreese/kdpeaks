32a592dfc68bdb8b0800e8044fd906b6c3e423f5
Marcus Breese <marcus@breese.com>
2020-02-14 22:38:51 -0500

    fix for 1D

--- R/kdpeakfinder.R
+++ R/kdpeakfinder.R
@@ -29,11 +29,125 @@ hello <- function() {
 
 #' @export
 find_peaks <- function(kd, combine_within=1, top_peak_z=1.65) {
+
+  if ("x" %in% names(kd) && "y" %in% names(kd) && "z" %in% names(kd)) {
+    # print("2d")
+    return(find_peaks_2d(kd, combine_within, top_peak_z))
+  }
+  if ("x" %in% names(kd) && "y" %in% names(kd)) {
+    # print("1d")
+    return(find_peaks_1d(kd, combine_within, top_peak_z))
+  }
+
+  stop("Bad density object: ", kd)
+}
+
+find_peaks_1d <- function(kd, combine_within=1, top_peak_z=1.65) {
+
+  flow.m <- rep(0, length(kd$x))
+
+  for (i in 1:length(kd$x)) {
+      # print(paste0("i=",i,", j=",j))
+      left = NA
+      me = kd$y[i]
+      right = NA
+
+      if (i > 1) {
+        left = kd$y[i-1]
+      }
+      if (i < length(kd$x)) {
+        right = kd$y[i+1]
+      }
+
+      flow.m[i] <- which(c(left, me, right)==max(c(left, me, right), na.rm = T))[1]
+
+  }
+
+
+  ## Find the peaks in order of magnitude
+  ##
+  ## Follow the flow established above to find the peaks
+  ## and then assign each position in the grid to a peak
+  ##
+
+
+  tmp.m <- kd$y
+
+  # peaks will have a flow.m value of 5. Everything else is ignored for this part
+  tmp.m[flow.m!=2] <- 0
+  peak.m <- rep(0, length(kd$x))
+
+  peak_nums <- c()
+  peak_cols <- c()
+  n <- 1
+
+  # Identify peaks in order of peak height.
+  while (max(tmp.m) > 0) {
+    max_peak <- max(tmp.m)
+    peak_i <- which(tmp.m == max_peak)
+    tmp.m[peak_i] <- 0
+    peak.m[peak_i] <- n
+
+    # look for adjacent peaks
+    #
+    # because we are going in order of peak height, we will merge
+    # any adjacent peaks to this one.
+
+    # print(paste0("Found peak at: [",peak_i,",",peak_j,"] ", sum(tmp.m > 0)))
+
+    for (i in max(1, peak_i-combine_within): min(length(kd$y), peak_i+combine_within)) {
+      if (tmp.m[i] > 0) {
+        tmp.m[i] <- 0
+        peak.m[i] <- n
+      }
+    }
+
+    peak_nums <- c(peak_nums, n)
+    peak_cols <- c(peak_cols, peak_i)
+
+    n <- n + 1
+  }
+  # print ("1")
+  ## assign each position to a peak
+  while(length(which(peak.m==0))>0) {
+    i <- which(peak.m==0)[1]
+    peak.m[i] <- recur_peak_1d(flow.m,i,peak.m)
+  }
+  # print ("2")
+
+  peak_size <- rep(0,length(peak_nums))
+  for (i in 1:length(kd$x)) {
+      peak <- peak.m[i]
+      peak_size[peak] <- peak_size[peak] + kd$y[i]
+  }
+  # print ("3")
+
+  peak_vals <- c()
+  for (i in 1:length(peak_cols)) {
+    peak_vals <- c(peak_vals, kd$y[peak_cols[i]])
+  }
+  # print ("4")
+
+  return(list(
+    num=peak_nums,
+    cols=peak_cols,
+    x=kd$x[peak_cols],
+    size=peak_size,
+    y=peak_vals,
+    pct=peak_size/sum(peak_size),
+    peak.m=peak.m,
+    top_peaks=sum(peak_vals > (mean(kd$y) + top_peak_z * sd(kd$y)))
+  ))
+}
+
+
+find_peaks_2d <- function(kd, combine_within=1, top_peak_z=1.65) {
+
   flow.m <- matrix(0, nrow=nrow(kd$z), ncol=ncol(kd$z))
 
   for (i in 1:nrow(kd$z)) {
     for (j in 1:ncol(kd$z)) {
-    #print(paste0("i=",i,", j=",j))
+    # print(paste0("i=",i,", j=",j))
       ul = NA
       top = NA
       ur = NA
@@ -97,22 +211,27 @@ find_peaks <- function(kd, combine_within=1, top_peak_z=1.65) {
   while (max(tmp.m) > 0) {
     max_peak <- max(tmp.m)
     peak_xy <- which(tmp.m == max_peak, arr.ind = TRUE)
-    peak_i <- peak_xy[1]
-    peak_j <- peak_xy[2]
+    #print(peak_xy)
+    peak_i <- peak_xy[1,1]
+    peak_j <- peak_xy[1,2]
     tmp.m[peak_i, peak_j] <- 0
     peak.m[peak_i, peak_j] <- n
+
+    # if (length(peak_rows) > 0 && peak_i == peak_rows[length(peak_rows)] && peak_j == peak_cols[length(peak_cols)] ) {
+    #   print(tmp.m)
+    # }
 
     # look for adjacent peaks
     #
     # because we are going in order of peak height, we will merge
     # any adjacent peaks to this one.
 
-    #print(paste0("Found peak at: [",peak_i,",",peak_j,"]"))
+    # print(paste0("Found peak at: [",peak_i,",",peak_j,"] ", sum(tmp.m > 0), " max: ", max_peak, ", tmp.m[]=",tmp.m[peak_i, peak_j]))
 
     for (i in max(1, peak_i-combine_within): min(nrow(kd$z), peak_i+combine_within)) {
       for (j in max(1, peak_j-combine_within): min(ncol(kd$z), peak_j+combine_within)) {
         if (tmp.m[i,j] > 0) {
-         # print(paste0("Combining peak[",i,",",j,"] with existing peak [",peak_i,",",peak_j,"]"))
+          # print(paste0("Combining peak[",i,",",j,"] with existing peak [",peak_i,",",peak_j,"]"))
           tmp.m[i,j] <- 0
           peak.m[i,j] <- n
         }
@@ -126,12 +245,15 @@ find_peaks <- function(kd, combine_within=1, top_peak_z=1.65) {
     n <- n + 1
   }
 
+  # print("1")
+
   ## assign each position to a peak
   while(length(which(peak.m==0, arr.ind = T))>0) {
     i <- which(peak.m==0, arr.ind = T)[1,1]
     j <- which(peak.m==0, arr.ind = T)[1,2]
-    peak.m[i,j] <- recur_peak(flow.m,i,j,peak.m)
+    peak.m[i,j] <- recur_peak_2d(flow.m,i,j,peak.m)
   }
+  # print("2")
 
   peak_size <- rep(0,length(peak_nums))
   for (i in 1:nrow(kd$z)) {
@@ -140,11 +262,13 @@ find_peaks <- function(kd, combine_within=1, top_peak_z=1.65) {
       peak_size[peak] <- peak_size[peak] + kd$z[i,j]
     }
   }
+  # print("3")
 
   peak_vals <- c()
   for (i in 1:length(peak_rows)) {
     peak_vals <- c(peak_vals, kd$z[peak_rows[i], peak_cols[i]])
   }
+  # print("4")
 
   return(list(
     num=peak_nums,
@@ -153,51 +277,69 @@ find_peaks <- function(kd, combine_within=1, top_peak_z=1.65) {
     x=kd$x[peak_rows],
     y=kd$y[peak_cols],
     size=peak_size,
-    vals=peak_vals,
+    z=peak_vals,
     pct=peak_size/sum(peak_size),
     peak.m=peak.m,
     top_peaks=sum(peak_vals > (mean(kd$z) + top_peak_z * sd(kd$z)))
   ))
-
 }
 
-recur_peak <- function(mat, row, col, ret.m) {
+recur_peak_2d <- function(mat, row, col, ret.m) {
   if (ret.m[row,col] != 0) {
     return (ret.m[row,col])
   }
   if (mat[row,col] == 1) {
-    v <- recur_peak(mat, row-1, col-1, ret.m)
+    v <- recur_peak_2d(mat, row-1, col-1, ret.m)
     return (v)
   }
   if (mat[row,col] == 2) {
-    v <- recur_peak(mat, row-1, col, ret.m)
+    v <- recur_peak_2d(mat, row-1, col, ret.m)
     return (v)
   }
   if (mat[row,col] == 3) {
-    v <- recur_peak(mat, row-1, col+1, ret.m)
+    v <- recur_peak_2d(mat, row-1, col+1, ret.m)
     return (v)
   }
   if (mat[row,col] == 4) {
-    v <- recur_peak(mat, row, col-1, ret.m)
+    v <- recur_peak_2d(mat, row, col-1, ret.m)
     return (v)
   }
   if (mat[row,col] == 5) {
     return (ret.m[row,col])
   }
   if (mat[row,col] == 6) {
-    v <- recur_peak(mat, row, col+1, ret.m)
+    v <- recur_peak_2d(mat, row, col+1, ret.m)
     return (v)
   }
   if (mat[row,col] == 7) {
-    v <- recur_peak(mat, row+1, col-1, ret.m)
+    v <- recur_peak_2d(mat, row+1, col-1, ret.m)
     return (v)
   }
   if (mat[row,col] == 8) {
-    v <- recur_peak(mat, row+1, col, ret.m)
+    v <- recur_peak_2d(mat, row+1, col, ret.m)
     return (v)
   }
   if (mat[row,col] == 9) {
-    v <- recur_peak(mat, row+1, col+1, ret.m)
+    v <- recur_peak_2d(mat, row+1, col+1, ret.m)
+    return (v)
+  }
+}
+
+
+
+recur_peak_1d <- function(v, i, ret.m) {
+  if (ret.m[i] != 0) {
+    return (ret.m[i])
+  }
+  if (v[i] == 1) {
+    v <- recur_peak_1d(v, i-1, ret.m)
+    return (v)
+  }
+  if (v[i] == 2) {
+    return(ret.m[i])
+  }
+  if (v[i] == 3) {
+    v <- recur_peak_1d(v, i+1, ret.m)
     return (v)
   }
 }
